a24ba209f7853d5cdc4f3331385c5bb892174c88
Ben Ray <bray22@gmail.com>
2017-07-19 00:08:26 -0400

    Add files via upload

---
+++ src/CoreHubServer/Controllers/LegacyHubRoutesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+
+namespace CoreHubServer.Controllers
+{
+    public class LegacyHubRoutesController : Controller
+    {
+        private IConfiguration config { get; set; }
+
+        public LegacyHubRoutesController(IConfiguration configuration)
+        {
+            this.config = configuration;
+        }
+
+        [Route("TheHub/{*.aspx}")]
+        [Route("/LandingPage.aspx")]
+        [Route("/Desktop.aspx")]
+        public IActionResult TheHub(string id)
+        {
+            return Redirect("/" + Request.QueryString.Value);
+        }
+
+        [Route("TheHubAPI/{*endpoint}")]
+        public IActionResult API(string endpoint)
+        {
+            return Redirect(config["AppSettings:OsEnvironmentUrl"] + "/TheHubAPI/" + endpoint + Request.QueryString.Value);
+        }
+
+        [Route("ATE_Report/{*endpoint}")]
+        [Route("TMSearchPortal/{*endpoint}")]
+        public IActionResult OsApps(string endpoint)
+        {
+            return Redirect(config["AppSettings:OsEnvironmentUrl"] + Request.Path.Value);
+        }
+
+    }
+}
